6b4e71bbbaee8eea119ef91d5280e2a96039d780
simrat12 <simratsaran@gmail.com>
2023-07-18 20:05:37 +0100

    structural changes for core program

--- programs/open_interest_protocol/src/lib.rs
+++ programs/open_interest_protocol/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Transfer, MintTo, Burn};
+use anchor_spl::token::{self, Transfer, MintTo, Burn, TokenAccount};
 pub mod state;
 use state::{PriceFeed, ErrorCode};
 use anchor_lang::solana_program::entrypoint::ProgramResult;
@@ -18,13 +18,13 @@ pub mod sol_anchor_contract {
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> ProgramResult {
         let cpi_accounts = Transfer {
             from: ctx.accounts.from.to_account_info().clone(),
-            to: ctx.accounts.to.to_account_info().clone(),
+            to: ctx.accounts.borrower_collateral_account.to_account_info().clone(),
             authority: ctx.accounts.user.to_account_info().clone(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info().clone();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::transfer(cpi_ctx, amount)?;
-
+    
         let borrower = &mut ctx.accounts.borrower;
         borrower.collateral += amount as f64;
         borrower.owner = *ctx.accounts.user.key;
@@ -33,26 +33,28 @@ pub mod sol_anchor_contract {
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
         let borrower = &mut ctx.accounts.borrower;
-        let from = &mut ctx.accounts.from;
+        let borrower_collateral_account = &mut ctx.accounts.borrower_collateral_account;
         let user = &ctx.accounts.user;
     
-        if from.owner != user.key {
+        if borrower_collateral_account.owner != *borrower.to_account_info().key {
             return Err(error!(ErrorCode::InvalidOwner));
         }
-
-        if borrower.collateral < (amount as f64) {
+    
+        // considering health factor and current loan
+        let max_withdrawable = borrower.collateral * borrower.health_factor - borrower.borrowed;
+        if max_withdrawable < amount as f64 {
             return Err(error!(ErrorCode::NotEnoughCollateral));
         }
-
+    
         let cpi_accounts = Transfer {
-            from: ctx.accounts.from.to_account_info().clone(),
+            from: ctx.accounts.borrower_collateral_account.to_account_info().clone(),
             to: ctx.accounts.to.to_account_info().clone(),
             authority: ctx.accounts.user.to_account_info().clone(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info().clone();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::transfer(cpi_ctx, amount)?;
-
+    
         borrower.collateral -= amount as f64;
         Ok(())
     }
@@ -62,15 +64,16 @@ pub mod sol_anchor_contract {
         let user = &ctx.accounts.user;
         let admin = &ctx.accounts.admin;
         
-        // Check if the user or admin is invoking this transaction.
         if borrower.owner != *user.key && admin.admin_pubkey != *user.key {
             return Err(error!(ErrorCode::Unauthorized));
         }
+    
         let loan_feed = &ctx.accounts.pyth_loan_account;
         let current_timestamp1 = Clock::get()?.unix_timestamp;
         let loan_price = loan_feed.get_price_no_older_than(current_timestamp1, 60).ok_or(ErrorCode::PythOffline)?;
         let loan_max_price = loan_price.price.checked_add(loan_price.conf as i64).ok_or(ErrorCode::Overflow)?;
-        let value_of_collateral = borrower.collateral * loan_max_price as f64; 
+        let value_of_collateral = borrower.collateral * loan_max_price as f64;
+    
         if value_of_collateral < (amount * 100) as f64 {
             return Err(error!(ErrorCode::NotEnoughCollateral));
         }
@@ -80,6 +83,7 @@ pub mod sol_anchor_contract {
             to: ctx.accounts.borrower_debt_token_account.to_account_info().clone(),
             authority: ctx.accounts.admin.to_account_info().clone(),
         };
+    
         let cpi_program = ctx.accounts.token_program.to_account_info().clone();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::mint_to(cpi_ctx, amount)?;
@@ -91,10 +95,9 @@ pub mod sol_anchor_contract {
     pub fn repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
         let borrower = &mut ctx.accounts.borrower;
         let user = &ctx.accounts.user;
-        let admin = &ctx.accounts.admin;
         
         // Check if the user or admin is invoking this transaction.
-        if borrower.owner != *user.key && admin.admin_pubkey != *user.key {
+        if borrower.owner != *user.key {
             return Err(error!(ErrorCode::Unauthorized));
         }
         if borrower.borrowed < (amount as f64) {
@@ -104,7 +107,7 @@ pub mod sol_anchor_contract {
         let cpi_accounts = Burn {
             mint: ctx.accounts.debt_token_mint.to_account_info().clone(),
             from: ctx.accounts.borrower_debt_token_account.to_account_info().clone(),
-            authority: ctx.accounts.admin.to_account_info().clone(),
+            authority: ctx.accounts.user.to_account_info().clone(),
         };
         let cpi_program = ctx.accounts.token_program.to_account_info().clone();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
@@ -141,93 +144,114 @@ pub struct Init<'info> {
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
-    /// CHECK: Ensure the `from` account is mutable so we can debit funds from it.
+    /// CHECK: Safe 
     #[account(mut)]
     pub from: AccountInfo<'info>,
-    /// CHECK: Ensure the `to` account is mutable so we can credit funds to it.
-    #[account(mut)]
-    pub to: AccountInfo<'info>,
-    /// CHECK: The `borrower` account needs to be mutable and its address needs
-    /// to be derived from the `borrower` constant, the user's public key, and the provided bump seed.
-    #[account(mut, seeds=[b"borrower".as_ref(), user.key.as_ref()], bump)]
+    /// CHECK: Safe
+    #[account(
+        mut,
+        seeds = [b"borrower_collateral".as_ref(), user.key.as_ref()],
+        bump,
+    )]
+    pub borrower_collateral_account: Account<'info, TokenAccount>,
+    /// CHECK: Safe
+    #[account(
+        init,
+        seeds = [b"borrower".as_ref(), user.key.as_ref()],
+        bump,
+        payer = user,
+        space = TokenAccount::LEN,
+    )]
     pub borrower: Account<'info, Borrower>,
-    /// CHECK: The `user` account needs to be mutable and its address needs
+    /// CHECK: Safe
+    pub system_program: AccountInfo<'info>,
+    /// CHECK: Safe
+    #[account(mut)]
     pub user: Signer<'info>,
-    /// CHECK: The `token_program` is not constrained since we assume it is the SPL Token program which does not require any specific checks.
+    /// CHECK: Safe
     pub token_program: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-    /// CHECK: The `borrower` account needs to be mutable and its address needs
-    /// to be derived from the `borrower` constant and the user's public key.
-    #[account(mut, seeds=[b"borrower".as_ref(), user.key.as_ref()], bump)]
+    /// CHECK: Safe
+    #[account(
+        seeds = [user.key().as_ref()],
+        bump,
+    )]
     pub borrower: Account<'info, Borrower>,
-    /// CHECK: Ensure the `from` account is mutable so we can debit funds from it.
-    #[account(mut)]
-    pub from: AccountInfo<'info>,
-    /// CHECK: Ensure the `to` account is mutable so we can credit funds to it.
+    /// CHECK: Safe
+    #[account(
+        mut,
+        constraint = borrower_collateral_account.owner == *borrower.to_account_info().key,
+    )]
+    pub borrower_collateral_account: Account<'info, TokenAccount>,
+    /// CHECK: Safe
     #[account(mut)]
     pub to: AccountInfo<'info>,
+    /// CHECK: Safe
     pub user: Signer<'info>,
-    /// CHECK: The `token_program` is not constrained since we assume it is the SPL Token program which does not require any specific checks.
+    /// CHECK: Safe
     pub token_program: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Borrow<'info> {
+    /// CHECK: Safe
     pub admin: Account<'info, Admin>,
-    /// CHECK: The `borrower` account needs to be mutable and its address needs
-    /// to be derived from the user's public key.
     #[account(
         seeds = [user.key().as_ref()],
         bump,
     )]
+    /// CHECK: Safe
     pub borrower: Account<'info, Borrower>,
-    /// CHECK: Ensure the `borrower_debt_token_account` account is mutable so we can credit funds to it.
+    // CHECK: Safe
     pub pyth_loan_account: Account<'info, PriceFeed>,
-    /// CHECK: Ensure the `borrower_debt_token_account` account is mutable so we can credit funds to it.
-    pub borrower_debt_token_account: Account<'info, Borrower>,
-    /// CHECK: Ensure the `debt_token_mint` account is mutable so we can debit funds from it.
-    pub debt_token_mint: Account<'info, Mint>,
-    /// CHECK: The `token_program` is not constrained since we assume it is the SPL Token program which does not require any specific checks.
+    /// CHECK: Safe
+    #[account(mut)]
+    pub borrower_debt_token_account: Account<'info, TokenAccount>,
+    /// CHECK: Safe
+    pub debt_token_mint: AccountInfo<'info>,
+    /// CHECK: Safe
     pub token_program: AccountInfo<'info>,
-    /// CHECK: The `user` account needs to be mutable and its address needs
+    /// CHECK: Safe
     pub user: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Repay<'info> {
-    pub admin: Account<'info, Admin>,
-    /// CHECK: The `borrower` account needs to be mutable and its address needs
-    /// to be derived from the user's public key.
+    /// CHECK: Safe
     #[account(
         seeds = [user.key().as_ref()],
         bump,
     )]
     pub borrower: Account<'info, Borrower>,
-    /// CHECK: Ensure the `borrower_debt_token_account` account is mutable so we can credit funds to it.
-    pub borrower_debt_token_account: Account<'info, Borrower>,
-    /// CHECK: Ensure the `debt_token_mint` account is mutable so we can debit funds from it.
-    pub debt_token_mint: Account<'info, Mint>,
-    /// CHECK: The `token_program` is not constrained since we assume it is the SPL Token program which does not require any specific checks.
+    /// CHECK: Safe
+    #[account(
+        mut,
+        constraint = borrower_debt_token_account.owner == *user.key,
+    )]
+    pub borrower_debt_token_account: Account<'info, TokenAccount>,
+    /// CHECK: Safe
+    pub debt_token_mint: AccountInfo<'info>,
+    /// CHECK: Safe
     pub token_program: AccountInfo<'info>,
-    /// CHECK: The `user` account needs to be mutable and its address needs
+    /// CHECK: Safe
     pub user: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct GetHealthFactor<'info> {
+    /// CHECK: Safe
     pub admin: Account<'info, Admin>,
     pub user: Signer<'info>,
-    /// CHECK: The `borrower` account needs to be mutable and its address needs
-    /// to be derived from the user's public key.
+    /// CHECK: Safe
     #[account(
         seeds = [user.key().as_ref()],
         bump,
     )]
     pub borrower: Account<'info, Borrower>,
-    /// CHECK: Ensure the `borrower_debt_token_account` account is mutable so we can credit funds to it.
+    /// CHECK: Safe
     pub pyth_collateral_account: Account<'info, PriceFeed>,
 }
 
@@ -246,46 +270,6 @@ pub struct Borrower {
     pub bump : u8,
 }
 
-#[derive(Clone)]
-#[derive(Accounts)]
-pub struct MintTokens<'info> {
-    /// CHECK: Ensure the `borrower` account is mutable so we can debit funds from it.
-    #[account(mut)]
-    pub borrower: AccountInfo<'info>,
-    /// CHECK: Ensure the `lender` account is mutable so we can credit funds to it.
-    #[account(mut)]
-    pub lender: AccountInfo<'info>,
-    /// CHECK: The `loan` account needs to be initialized and the `borrower` needs to be the payer for it.
-    #[account(init, payer = borrower, space=9000)]
-    pub loan: Account<'info, Loan>,
-    /// CHECK: Ensure `mint` is mutable and that its `mint_authority` matches the `mint_authority`'s key.
-    #[account(
-        mut,
-        constraint = mint.mint_authority == *mint_authority.key,
-    )]
-    pub mint: Account<'info, Mint>,
-    #[account(signer)]
-    /// CHECK: The `mint_authority` account needs to be mutable and its address needs
-    pub mint_authority: AccountInfo<'info>,
-    /// CHECK: The `system_program` is not constrained since we assume it is the System program which does not require any specific checks.
-    pub system_program: AccountInfo<'info>,
-    /// CHECK: The `token_program` is not constrained since we assume it is the SPL Token program which does not require any specific checks.
-    pub token_program: AccountInfo<'info>,
-}
-
-#[account]
-pub struct Loan {
-    borrower: Pubkey,
-    lender: Pubkey,
-    amount: u64,
-    mint: Pubkey,
-}
-
-#[account]
-pub struct Mint {
-    mint_authority: Pubkey,
-    supply: u64,
-}
 
 
 
